7163fb526256f682fd4fc0d1b1df0c0cb4b93e8c
Bernhard K. Weisshuhn <bkw@codingforce.com>
2012-12-26 15:59:35 +0100

    add requestAnimationFrame shim

--- dist/nodecopter-stream.js
+++ dist/nodecopter-stream.js
@@ -1,5 +1,45 @@
 /*jshint browser:true */
-/*global Avc:true, YUVWebGLCanvas: true, Size: true */
+/*global Avc:true, YUVWebGLCanvas: true, Size: true, requestAnimationFrame:true */
+
+/* requestAnimationFrame polyfill: */
+(function (window) {
+    'use strict';
+    var lastTime = 0,
+        vendors = ['ms', 'moz', 'webkit', 'o'],
+        x,
+        length,
+        currTime,
+        timeToCall;
+
+    for (x = 0, length = vendors.length; x < length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[
+            vendors[x] + 'RequestAnimationFrame'
+        ];
+        window.cancelAnimationFrame = window[
+            vendors[x] + 'CancelAnimationFrame'
+        ] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (callback, element) {
+            currTime = new Date().getTime();
+            timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            lastTime = currTime + timeToCall;
+            return window.setTimeout(function () {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+        };
+    }
+
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+    }
+}(window));
+
+
+/* NodeCopterStream: */
 (function (window, document, undefined) {
     'use strict';
     var NS,
