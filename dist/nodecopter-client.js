8add9fc12b0cf28024979c855c4fec6237e50825
Bernhard K. Weisshuhn <bkw@codingforce.com>
2012-12-26 16:15:10 +0100

    build new client

--- dist/nodecopter-client.js
+++ dist/nodecopter-client.js
@@ -1098,7 +1098,47 @@ var FilterWebGLCanvas = (function () {
   
   return constructor;
 })(); /*jshint browser:true */
-/*global Avc:true, YUVWebGLCanvas: true, Size: true */
+/*global Avc:true, YUVWebGLCanvas: true, Size: true, requestAnimationFrame:true */
+
+/* requestAnimationFrame polyfill: */
+(function (window) {
+    'use strict';
+    var lastTime = 0,
+        vendors = ['ms', 'moz', 'webkit', 'o'],
+        x,
+        length,
+        currTime,
+        timeToCall;
+
+    for (x = 0, length = vendors.length; x < length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[
+            vendors[x] + 'RequestAnimationFrame'
+        ];
+        window.cancelAnimationFrame = window[
+            vendors[x] + 'CancelAnimationFrame'
+        ] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (callback, element) {
+            currTime = new Date().getTime();
+            timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            lastTime = currTime + timeToCall;
+            return window.setTimeout(function () {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+        };
+    }
+
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+    }
+}(window));
+
+
+/* NodeCopterStream: */
 (function (window, document, undefined) {
     'use strict';
     var NS,
@@ -1122,12 +1162,14 @@ var FilterWebGLCanvas = (function () {
     }
 
     function handleDecodedFrame(buffer, width, height) {
-        var lumaSize = width * height,
+        requestAnimationFrame(function () {
+            var lumaSize = width * height,
             chromaSize = lumaSize >> 2;
-        webGLCanvas.YTexture.fill(buffer.subarray(0, lumaSize));
-        webGLCanvas.UTexture.fill(buffer.subarray(lumaSize, lumaSize + chromaSize));
-        webGLCanvas.VTexture.fill(buffer.subarray(lumaSize + chromaSize, lumaSize + 2 * chromaSize));
-        webGLCanvas.drawScene();
+            webGLCanvas.YTexture.fill(buffer.subarray(0, lumaSize));
+            webGLCanvas.UTexture.fill(buffer.subarray(lumaSize, lumaSize + chromaSize));
+            webGLCanvas.VTexture.fill(buffer.subarray(lumaSize + chromaSize, lumaSize + 2 * chromaSize));
+            webGLCanvas.drawScene();
+        });
     }
 
     function setupCanvas(div) {
@@ -1151,7 +1193,7 @@ var FilterWebGLCanvas = (function () {
         socket = new WebSocket(
              'ws://' +
             window.document.location.hostname + ':' +
-            window.document.location.port
+            window.document.location.port + '/dronestream'
         );
         socket.binaryType = 'arraybuffer';
         socket.onmessage = handleNalUnits;
